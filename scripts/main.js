0113d466f36b4c6b7837b6793a7f08c0a6339ea1
Jonathan Ochoa <ochoajonathan@icloud.com>
2019-04-10 21:00:01 -0400

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  var mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/main-image.png') {
+    myImage.setAttribute ('src','images/second-image.png');
+  } else {
+    myImage.setAttribute ('src','images/main-image.png');
+  }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please Enter Your Name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Player Trade House, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Player Trade House, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
